99bb087b957a7c165874650858f9b3c6825d28a2
Teddy Rasmussen <teddyjraz@gmail.com>
2021-11-12 09:05:59 -0700

    debug

--- static/redirect.js
+++ static/redirect.js
@@ -41,13 +41,13 @@ function redirectToTheme(cookie_name, document) {
         }
         if (c.indexOf(name) == 0) {
         let fullThemeCookie = c.substring(name.length, c.length);
-        console.log(window.location.href.substring(43))
+        console.log(window.location.href.substring(43, 44))
         console.log(fullThemeCookie)
-        if (window.location.href.substring(-2) != fullThemeCookie && window.location.href.includes("login") == false) {
+        if (window.location.href.substring(43, 44) != fullThemeCookie && window.location.href.includes("login") == false) {
             return window.location.assign("https://tech-catalog-nuxt.vercel.app/theme" + fullThemeCookie + window.location.href.substring(-2));
         }
-        else if (window.location.href.substring(-2) != fullThemeCookie && window.location.href.includes("login") == true) {
-            console.log(window.location.href.substring(-2));
+        else if (window.location.href.substring(43, 44) != fullThemeCookie && window.location.href.includes("login") == true) {
+            console.log(window.location.href.substring(43, 44));
             // return window.location.assign("https://tech-catalog-nuxt.vercel.app/theme" + fullThemeCookie);
         }
         else {

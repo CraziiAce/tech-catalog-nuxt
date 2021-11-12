48056bd122fe271891a851f974d4da818da661e6
Teddy Rasmussen <teddyjraz@gmail.com>
2021-11-11 19:59:00 -0700

    debug

--- static/redirect.js
+++ static/redirect.js
@@ -41,6 +41,8 @@ function redirectToTheme(cookie_name, document) {
         }
         if (c.indexOf(name) == 0) {
         let fullThemeCookie = c.substring(name.length, c.length);
+        console.log(window.location.href.substring(-1) != fullThemeCookie && window.location.href.includes("login"))
+        console.log(window.location.href.substring(-1))
         if (window.location.href.substring(-1) != fullThemeCookie && window.location.href.includes("login") == false) {
             return window.location.assign("https://tech-catalog-nuxt.vercel.app/theme" + fullThemeCookie + window.location.href.substring(43));
         }

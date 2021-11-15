18583f24c5cce5a72c5c6e047fa53be79524b2f8
Teddy Rasmussen <teddyjraz@gmail.com>
2021-11-15 12:17:54 -0700

    test catalog item link

--- components/theme3/catalogItem.vue
+++ components/theme3/catalogItem.vue
@@ -2,13 +2,14 @@
   <figure
     class="rounded-xl border-4 m-12 p-2 h-auto bg-gray-800 text-gray-50 border-gray-400"
   >
-    <a :href="id_url"></a>
-    <h1 class="text-center font-light " style="font-family: prometo;">
-      {{ name }}
-    </h1>
-    <div class="w-full h-full">
-      <img :src="img" :alt="name" class="w-auto h-auto align-center" />
-    </div>
+    <a :href="id_url">
+      <h1 class="text-center font-light " style="font-family: prometo;">
+        {{ name }}
+      </h1>
+      <div class="w-full h-full">
+        <img :src="img" :alt="name" class="w-auto h-auto align-center" />
+      </div>
+    </a>
   </figure>
 </template>
 <script>

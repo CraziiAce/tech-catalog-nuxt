c9f44e305ffd33735b6e3265fb1c58809398291c
Teddy Rasmussen <teddyjraz@gmail.com>
2021-11-15 10:44:27 -0700

    change fonts

--- components/theme3/catalogItem.vue
+++ components/theme3/catalogItem.vue
@@ -2,7 +2,7 @@
   <figure
     class="rounded-xl border-4 m-12 p-2 h-auto bg-gray-800 text-gray-50 border-gray-400"
   >
-    <h1 class="text-center font-black " style="font-family: prometo;">
+    <h1 class="text-center font-light " style="font-family: prometo;">
       {{ name }}
     </h1>
     <div class="w-full h-full">

13fa41db7065e624603e7650007a67274674dc7f
Teddy Rasmussen <teddyjraz@gmail.com>
2021-11-15 10:36:09 -0700

    mess with colors

--- components/theme3/catalogItem.vue
+++ components/theme3/catalogItem.vue
@@ -1,5 +1,7 @@
 <template>
-  <figure class="rounded-xl border-4 m-12 p-2 h-auto bg-gray-800 text-gray-50">
+  <figure
+    class="rounded-xl border-4 m-12 p-2 h-auto bg-gray-800 text-gray-50 border-gray-400"
+  >
     <h1 class="text-center font-black " style="font-family: acumin-pro;">
       {{ name }}
     </h1>

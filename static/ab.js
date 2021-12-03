6f9cd7ada86d6959fbba7f9c3e61a8d766bb6611
Teddy Rasmussen <teddyjraz@gmail.com>
2021-12-03 13:39:28 -0700

    track client ip

--- static/ab.js
+++ static/ab.js
@@ -1,16 +1,23 @@
 async function pageLoad(document) {
-    const event = await fetch(
-        "https://tech-catalog-backend.herokuapp.com/record_event",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            // get value of cookie "theme"
-            "theme": document.cookie.split("theme=")[1].split(";")[0],
-            "time": Date.now().toString(),
-            "event": "page_view",
-            "session_token": document.cookie.split("session_token=")[1].split(";")[0],
-          })
-        }
-      ).then(res => res.json());
+
+  const client_ip = await fetch(
+    'https://api.ipify.org?format=json'
+  ).then(res => res.json());
+
+  const event = await fetch(
+    "https://tech-catalog-backend.herokuapp.com/record_event",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        // get value of cookie "theme"
+        "theme": document.cookie.split("theme=")[1].split(";")[0],
+        "time": Date.now().toString(),
+        "event": "page_view",
+        "session_token": document.cookie.split("session_token=")[1].split(";")[0],
+        "client_ip": client_ip.ip
+      })
+    }
+  ).then(res => res.json());
+console.log(event);
     console.log(event);
 }

f0fc1e3dace2a5940868647dcbc367db12841351
Teddy Rasmussen <teddyjraz@gmail.com>
2022-01-10 09:50:14 -0700

    test new category thing

--- pages/theme2/index.vue
+++ pages/theme2/index.vue
@@ -25,15 +25,33 @@ export default {
   components: { catalogItem },
   mounted() {
     checkCookie("session_token", document);
-  },
-  async asyncData() {
-    const items = await fetch(
+    const res = await fetch(
       "https://tech-catalog-backend.herokuapp.com/get_items"
-    ).then(res => res.json());
+    );
+    let items = await res.json();
 
+    let categories = ["all"];
+
+    items.forEach(iter)
+
+    function iter(value) {
+      value = value['categories'].split(', ');
+      value.forEach(checkCategories);
+    }
+    function checkCategories(value) {
+      if (!categories.includes(value)) {
+        categories.push(value);
+      }
+    }
     return {
-      items: items
+      items: items,
+      categories: categories,
     };
+  },
+  methods: {
+    isInclude(name, array) {
+      return array.includes(name);
+    }
   }
 };
 </script>
